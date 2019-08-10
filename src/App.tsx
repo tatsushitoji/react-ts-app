25052ca9871e71ed30f44afddb3338331c59409f
Tatsushi Toji <28854043+tatsushitoji@user.noreply.github.com>
2019-08-10 16:59:40 +0900

    :tada: first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { hot } from 'react-hot-loader';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App: React.FC = () => (
+  <div>Hello App !!!!!</div>
+);
+
+export default hot(module)(App);
---
+++ webpack.config.ts
@@ -0,0 +1,107 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const TerserPlugin = require('terser-webpack-plugin');
+
+// @ts-ignore
+module.exports  = (_, { mode }: { mode: string }) => {
+  const productionMode = mode === 'production'
+  const base = {
+    entry: ['react-hot-loader/patch', './src/index.tsx'],
+
+    output: {
+      filename: '[name].[hash].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+    },
+
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"],
+    },
+
+    module: {
+      rules: [
+        { test: /\.tsx?$/, loader: "babel-loader" },
+        // {
+        //   test: /\.(sa|sc|c)ss$/,
+        //   use: [
+        //     devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+        //     'css-loader',
+        //   ],
+        // },
+        // {
+        //   test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        //   use: [{
+        //     loader: 'file-loader',
+        //     options: {
+        //       name: '[name].[ext]',
+        //       outputPath: 'fonts',
+        //     }
+        //   }]
+        // }
+      ]
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: 'src/index.html',
+      }),
+      // new MiniCssExtractPlugin({
+      //   filename: devMode ? '[name].css' : '[name].[hash].css',
+      //   chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      // }),
+    ],
+
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+            enforce: true,
+          }
+        },
+      }
+    },
+
+    // node: {
+    //   fs: 'empty',
+    // },
+  };
+
+  const development = merge(base, {
+    devtool: "cheap-module-eval-source-map",
+    devServer: {
+      stats: { colors: true },
+      port: 3000,
+      historyApiFallback: true,
+      open: true,
+    },
+  });
+
+  // const production = merge(base, {
+  //   plugins: [
+  //     new BundleAnalyzerPlugin({ analyzerMode: 'static' }),
+  //   ],
+  //   optimization: {
+  //     minimizer: [
+  //       new TerserPlugin({
+  //         terserOptions: {
+  //           sourceMap: true,
+  //           parallel: true,
+  //           compress: {
+  //             collapse_vars: false, // workaround for a minifier's bug: https://github.com/terser-js/terser/issues/120
+  //             drop_console: true,
+  //           },
+  //         },
+  //       }),
+  //     ],
+  //   }
+  // });
+
+  const config = development
+  return config
+}
