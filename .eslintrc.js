e20a7829011273df93a986719da59b1ba950e315
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2019-08-12 22:45:47 +0900

    :recycle: add some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,19 @@ module.exports = {
   plugins: ['@typescript-eslint', 'react-hooks'],
   rules: {
     'react-hooks/rules-of-hooks': 'error',
-
+    '@typescript-eslint/explicit-function-return-type': "off",
+    "@typescript-eslint/prefer-interface": "off",
+    "import/prefer-default-export": "off",
+    "import/no-unresolved": "off",
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error", {
+      "vars": "all",
+      "args": "after-used",
+      "ignoreRestSiblings": false
+    }],
   },
   settings: {
     react: {
